3de0be8c07403d817095122913c195357fd92a45
Smita Upadhyay <smitaupadhyay@Smitas-MacBook-Pro.local>
2018-01-11 20:55:50 -0600

    test

--- data/sm1.R
+++
@@ -1,4 +0,0 @@
-library(readxl)
-irs2015 <- read_excel("data/irs_2015.xls", skip = 7)
-head(irs2015)
-View(irs2015)
---
+++ sm1.R
@@ -0,0 +1,8 @@
+setwd("/Users/smitaupadhyay/Desktop/data_science01/repogit/data-question-4-data-question-4-riot-oracles")
+library(readxl)
+irs2015 <- read_excel("data/irs_2015.xls", skip = 7)
+head(irs2015)
+View(irs2015)
+zip_TN <- read.csv("data/zip_TN.csv")
+
+#testing commit for branch
