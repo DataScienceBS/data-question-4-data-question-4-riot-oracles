446000090dec2a8b2ab24ceb8eb1a4079ea106db
jroth006 <jroth006@gmail.com>
2018-01-20 13:50:53 -0600

    exploring impact of econ dis and bhn

--- exploratory_analysis.R
+++ exploratory_analysis.R
@@ -7,6 +7,62 @@ load("data/merged_df.rds")
 by_agi_range <- merged_df %>% 
   filter(zip_code != 0 & !is.na(agi_range))
 
+## Total across zip code - removes agi ranges
+zip_total <- merged_df %>% 
+  filter(zip_code != 0 & is.na(agi_range))
+
+## Filtering df to show the agi range for each zip code
+bhn_grad <- merged_df %>% 
+  filter(zip_code != 0 & !is.na(Pct_BHN)) %>% 
+  group_by(zip_code) %>% 
+  summarise(avg_BHN = mean(Pct_BHN),
+            avg_Graduation = mean(Graduation))
+
+ggplot(bhn_grad, aes(x = avg_BHN, y = avg_Graduation)) +
+  geom_point(alpha = .3)
+
+## Exploring the relationship between % of minority students with other variables:
+bhn_agi_plot <- zip_total %>% 
+  filter(agi_a > 0) %>% 
+  mutate(avg_agi = (agi_a * 1000) / return_c,
+         avg_unemp = (unemp_comp_c / return_c)) %>% 
+  group_by(system_name) %>% 
+  summarise_all(funs(mean))
+
+## Code for exploratory analysis
+pairs(~AlgI + AlgII + Math + BioI + Chemistry + Science + Pct_ED, data = bhn_agi_plot)
+
+bhn_lm <- lm(mean_bhn ~ mean_act, data = bhn_agi_plot, na.action = na.exclude)
+plot(bhn_lm)
+
+ggplot(bhn_agi_plot, aes(x = avg_agi, y = avg_unemp)) +
+  geom_point(alpha = 0.3)
+
+cor.test(bhn_agi_plot$AlgI, bhn_agi_plot$Pct_ED, method = "pearson")
+
+## Correlation scores for Pct_BHN with:
+## Pct_Suspended = 0.6944
+## Graduation = -0.3064
+## Pct_Chronically_Absent = 0.0245
+## avg_agi = 0.3505
+## ACT_Composite = -0.2214
+## Dropout = 0.2709
+## Math = -0.2098
+## Science = -0.3804
+## Pct_ED = 0.2343
+
+## Correlations with Pct_ED:
+## avg_unemp = -0.4314
+## 
+
+subjects_df <- zip_total %>% 
+  select(system_name, AlgI, AlgII, Math, 
+         BioI, Chemistry, Science, Pct_ED) %>%
+  group_by(system_name) %>% 
+  summarise_all(funs(mean))
+
+PerformanceAnalytics::chart.Correlation(subjects_df[2:8], histogram = TRUE, pch = 19)
+
 ## Avg by_agi_range to show mean AGI and graduation rates by zip, including CORE region
 agi_grad_plot <- by_agi_range %>% 
   filter(agi_a > 0,
@@ -19,12 +75,12 @@ agi_grad_plot <- by_agi_range %>%
   filter(!is.na(CORE_region))
 
 ## Scatter
-#ggplot(agi_grad_plot, aes(x = mean_agi, y = mean_grad, color = CORE_region)) +
-#  geom_point(alpha = 0.5)
+ggplot(agi_grad_plot, aes(x = mean_agi, y = mean_grad, color = CORE_region)) +
+ geom_point(alpha = 0.5)
 
 ## Boxplot
-# ggplot(agi_grad_plot, aes(x = CORE_region, y = mean_grad)) +
-#   geom_boxplot()
+ggplot(agi_grad_plot, aes(x = CORE_region, y = mean_grad)) +
+  geom_boxplot()
 
 ## Southwest/Memphis CORE shows a wide variation in graduation rates, so exploring further
 southwest_core <- by_agi_range %>%
@@ -58,20 +114,29 @@ overall_score <- by_agi_range %>%
             mean_engIII = mean(EngIII)
             )
 
+# ## Function that doesn't work, sadly.
+# score_plot <- function(df, score1, score2) {
+#   ggplot(df, aes(x = score1, y = score2)) +
+#   geom_point(alpha = 0.5)
+# }
+# 
+# ## Function calls that failed
+# mean_algI <- score_plot(overall_score, mean_algI, mean_chr_absent)
+# mean_algII <- score_plot(overall_score, mean_algII, mean_chr_absent)
+# mean_math <- score_plot(overall_score, mean_math, mean_chr_absent)
+# mean_bioI <- score_plot(overall_score, mean_bioI, mean_chr_absent)
+# mean_chemistry <- score_plot(overall_score, mean_chemistry, mean_chr_absent)
+# mean_science <- score_plot(overall_score, mean_science, mean_chr_absent)
+# mean_ela <- score_plot(overall_score, mean_ela, mean_chr_absent)
+# mean_engI <- score_plot(overall_score, mean_engI, mean_chr_absent)
+# mean_engII <- score_plot(overall_score, mean_engII, mean_chr_absent)
+# mean_engIII <- score_plot(overall_score, mean_engIII, mean_chr_absent)
+# 
+# ggplot(overall_score, aes(x = mean_algI, y = mean_chr_absent)) +
+#   geom_point(alpha = 0.5)
+# ggplot(overall_score, aes(x = mean_algII, y = mean_chr_absent)) +
+#   geom_point(alpha = 0.5)
+# ggplot(overall_score, aes(x = mean_math, y = mean_chr_absent)) +
+#   geom_point(alpha = 0.5)
 
-score_plot <- function(df, score1, score2)
-  ggplot(df, aes(x = score1, y = score2)) +
-  geom_point(alpha = 0.5)
 
-mean_algI <- score_plot(overall_score, mean_algI, mean_chr_absent)
-mean_algII <- score_plot(overall_score, mean_algII, mean_chr_absent)
-mean_math <- score_plot(overall_score, mean_math, mean_chr_absent)
-mean_bioI <- score_plot(overall_score, mean_bioI, mean_chr_absent)
-mean_chemistry <- score_plot(overall_score, mean_chemistry, mean_chr_absent)
-mean_science <- score_plot(overall_score, mean_science, mean_chr_absent)
-mean_ela <- score_plot(overall_score, mean_ela, mean_chr_absent)
-mean_engI <- score_plot(overall_score, mean_engI, mean_chr_absent)
-mean_engII <- score_plot(overall_score, mean_engII, mean_chr_absent)
-mean_engIII <- score_plot(overall_score, mean_engIII, mean_chr_absent)
-
-grid
