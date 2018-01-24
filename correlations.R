d91b326506fbae15f44070f1eed0c020655444ec
Brandon Sanders <DataScienceBS@gmail.com>
2018-01-23 20:52:52 -0600

    SUCCESS! ACT Composite Predicted!

--- correlations.R
+++ correlations.R
@@ -1,7 +1,7 @@
+# SUCCESS! Removed DeKalb County from training data and was able to accurately predict ACT Composite Score using ACME (AlgII, Chem, Math, ELA)
 # correlation between Dropout and ACT Composite is -0.336, removing 1 outlier ACT<15 adjusts to -0.243
 # correlation between Dropout and Per Pupil Expenditures is 0.274
 # correlation between Pct_ED (Econ Disadv) and Per Pupil Expenditures is 0.157
-
 
 library(tidyr)
 library(dplyr)
@@ -10,158 +10,6 @@ library(ggplot2)
 combined_df <- readRDS("combined_df.RDS")
 merged_df <- readRDS("merged_df.RDS")
 school_cross <- readRDS("school_cross.RDS")
-
-#### Adding AGI Amount per Return ####
-
-combined_df %<>% 
-  filter(return_c > 0) %>% 
-  mutate(agi_per_return = agi_a / return_c)  
-## consider multiplying by 1k ##
-
-    
-##################################
-#    starting plot potential     #
-##################################
-
-
-combined_df %>%
-  filter(return_c > 0) %>% 
-  mutate(agi_per_return = (agi_a / return_c)) %>% 
-  group_by(county, agi_range, agi_per_return) %>% 
-  summarise(avg_act = mean(ACT_Composite),
-            avg_agi = mean(agi_per_return)) %>% 
-  ggplot(., aes( x=avg_act, y=avg_agi, color=agi_range)) +
-  geom_point()
-
-
-combined_df %>% 
-  filter(zip_code != 0 | zip_code != 99999) %>% 
-  filter(agi_range != 'Total') %>% 
-  group_by(agi_range) %>% 
-  ggplot(., aes(x=agi_range, y=agi_a)) +
-  geom_point()
-
-
-#### boxplot for ACT scores as a function of CORE Region ####
-combined_df %>% 
-  filter(zip_code != 0 | zip_code != 99999) %>% 
-  filter(agi_range != 'Total') %>% 
-  ggplot(., aes(x=CORE_region, y=ACT_Composite)) +
-  geom_boxplot() +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
-
-
-###########################
-#### mapping Tennessee ####
-###########################
-# help from: http://eriqande.github.io/rep-res-web/lectures/making-maps-with-R.html
-###########################
-###      data prep      ###
-###########################
-
-library(maps)
-library(mapdata)  #preloaded state/county coordinates
-
-TN_data <- map_data("state") %>% 
-  filter(region =='tennessee')
-
-TN_counties <- map_data("county") %>% subset(., region == "tennessee")
-
-
-##########################################
-# converting tax$county to lcase for map #
-##########################################
-combined_df$county_l <- sapply(combined_df$county, tolower) %>% gsub(' county','',.)
-tax_school_map_df <- left_join(combined_df, TN_counties, by=c('county_l'='subregion')) 
-##########################################
-
-TN_map <- ggplot(data = TN_data, mapping = aes(x = long, y = lat, group = group)) + 
-  coord_fixed(1.3) +  # prevents axis skew
-  geom_polygon(data = TN_counties, fill = "orange", color = "white") +  #county fill and borders
-  geom_polygon(color = "black", fill = NA) #state outline and fill
-
-### need to remove plot background ###
-
-TN_map
-
-##################################
-###   Top 10 counties by pop   ###
-##################################
-top_10_pop <- zip_codes %>%  
-  group_by(county) %>% 
-  summarize(sum_pop = sum(irs_estimated_population_2014, na.rm = TRUE)) %>% 
-  filter(sum_pop > 100000) %>% 
-  arrange(desc(sum_pop)) %>% 
-  head(n=10)  
-
-top_10_pop  
-
-
-##########################################################
-#  join tax/count_l before mapping,  fill counties by:  
-# 
-# 1. Group By county, sum/avg with enrollment weighted
-# 2. avg. AGI amt per return filed
-# 3. ACT composite scores
-# 4. % of returns in AGI Range (lowest, highest
-# 5. dropout rate
-# 6. graduation rate 
-# 7. total Math / Science 
-# 8. % non-white
-# 9. find correlations, colorfill Top/Bottom 10
-#   9a. percentage of AGI Group count per county => Top 10, corr w/ graduation rate 
-#   9b. percentage of AGI group, Top 10/Bottom 10, corr w/  ACT composite 
-#   9c. 3-dimensional heat map, x = Percentage, y = AGI Group, z = Graduation Rate | ACT Comp
-# 
-########################################################## 
-
-###################
-## pairs testing ##
-###################
-
-pairs(~ agi_per_return + ACT_Composite + ratio_by_agi + Dropout, data=combined_df)
-
-# scatter with RATIO_BY_AGI and ACT score #
-combined_df %>% 
-  filter(zip_code != 0 | zip_code != 99999) %>% 
-  filter(agi_range != 'Total') %>% 
-  #  group_by(agi_range) %>% 
-  ggplot(., aes(x=ratio_by_agi, y=ACT_Composite, color=CORE_region)) +
-  geom_point() +
-  facet_grid(. ~ agi_range)
-# the county ratio by agi group does not visually appear to have correlation with ACT scores.
-
-# scatter with RATIO_BY_AGI and DROPOUT #
-combined_df %>% 
-  filter(zip_code != 0 | zip_code != 99999) %>% 
-  filter(agi_range != 'Total') %>% 
-  #  group_by(agi_range) %>% 
-  ggplot(., aes(x=ratio_by_agi, y=Dropout, color=CORE_region)) +
-  geom_point() +
-  facet_grid(. ~ agi_range)
-# the county ratio by agi group does not visually appear to have correlation with ACT scores.
-
-
-# scatter with ACT and Dropout, facet by CORE, color by AGI_Range
-combined_df %>% 
-  filter(zip_code != 0 | zip_code != 99999) %>% 
-  filter(agi_range != 'Total') %>% 
-  filter(CORE_region != 'NA') %>% 
-  #  group_by(agi_range) %>% 
-  ggplot(., aes(x=ACT_Composite, y=Dropout, color=agi_range)) +
-  geom_point() +
-  facet_grid(. ~ CORE_region)
-
-
-combined_df %>% 
-  filter(zip_code != 0 | zip_code != 99999) %>% 
-  filter(agi_range != 'Total') %>% 
-  filter(CORE_region != 'NA') %>% 
-  #  group_by(agi_range) %>% 
-  ggplot(., aes(x=ACT_Composite, y=Dropout, color=agi_range)) +
-  geom_point()
-
-
 
 # -0.546 correlation between ACT Composite and Dropout rate #
 ## 95% confidence interval:  -0.564, -0.527 ##
@@ -231,20 +79,6 @@ cor.test(school_cross$ACT_Composite, school_cross$EngI, method = 'pearson')   # 
 cor.test(school_cross$ACT_Composite, school_cross$EngII, method = 'pearson')  # 0.734
 cor.test(school_cross$ACT_Composite, school_cross$EngIII, method = 'pearson') # 0.605
 
-# first iteration of model #
-model_full <- lm(formula = ACT_Composite ~ AlgI + AlgII + Math + Science + BioI + Chemistry + ELA + EngI + EngII + EngIII, data = school_cross)
-plot(model_full)
-summary(model_full)
-
-# tweaking model for best features # 
-#  ACME = AlgII, Chem, Math, ELA   #
-model_acme <- lm(formula = ACT_Composite ~ AlgII + Math + Chemistry + ELA, data = school_cross)
-summary(model_acme)
-plot(model_acme)
-
-# working to identify outliers from the plots #
-school_cross[c(18,30,40),]
-
 # correlate ACT with non-academic features
 cor.test(school_cross$ACT_Composite, school_cross$Graduation, method = 'pearson')             # 0.357
 cor.test(school_cross$ACT_Composite, school_cross$Pct_Chronically_Absent, method = 'pearson') # -0.392
@@ -255,6 +89,37 @@ cor.test(school_cross$ACT_Composite, school_cross$Pct_Hispanic, method = 'pearso
 cor.test(school_cross$ACT_Composite, school_cross$Pct_Black, method = 'pearson')      # -0.251
 cor.test(school_cross$ACT_Composite, school_cross$Pct_Native_American, method = 'pearson') # 0.257
 
+
+##############################################
+#     function to remove rows, for outliers  #
+#         and for prediction purposes.       #
+#     Don't want test data in training data  #
+##############################################
+remove_rows <- function(x, rows) x[-rows,, drop = FALSE]
+school_cross_no_dekalb <- remove_rows(school_cross,37)
+
+# first iteration of model (model_full) #
+model_full <- lm(formula = ACT_Composite ~ AlgI + AlgII + Math + Science + BioI + Chemistry + ELA + EngI + EngII + EngIII, data = school_cross_no_dekalb)
+plot(model_full)
+summary(model_full)
+
+# tweaking model for best features (model_acme) #  ACME = AlgII, Chem, Math, ELA   #
+model_acme <- lm(formula = ACT_Composite ~ AlgII + Math + Chemistry + ELA, data = school_cross_no_dekalb)
+summary(model_acme)
+plot(model_acme)
+
+# plots suggest 3 main outliers: 18, 30, 40. Removing and creating new models #
+remove_rows <- function(x, rows) x[-rows,, drop = FALSE]
+school_cross_no_dekalb_no_outliers <- remove_rows(school_cross_no_dekalb, c(18,30,40))
+
+### redo model_full and model_acme without 3 outliers ###
+model_full_2 <- lm(formula = ACT_Composite ~ AlgI + AlgII + Math + Science + BioI + Chemistry + ELA + EngI + EngII + EngIII, data = school_cross_no_dekalb_no_outliers)
+plot(model_full_2)
+summary(model_full_2)
+
+model_acme_2 <- lm(formula = ACT_Composite ~ AlgII + Math + Chemistry + ELA, data = school_cross_no_dekalb_no_outliers)
+summary(model_acme_2)
+plot(model_acme_2)
 
 #########################################################################   
 ######  testing prediction data based on 4 proficiency categories  ######
@@ -271,12 +136,25 @@ predict(model_acme, test_acme_hi)
 predict(model_acme, dekalb_acme)
 predict(model_full,dekalb_full)
 #########################################################################   
-######  using DeKalb County, ACME model estimates 17.7 ACT Score   ######
+######  using DeKalb County, ACME model estimates 19.1 ACT Score   ######
 ######  using DeKalb County, full model estimates 19.4 ACT Score   ######
 ######         Actual DeKalb County ACT Score was 19.1             ######
 #########################################################################
+######            removing 3 outliers from the data...             ######
+#########################################################################
+predict(model_full_2,dekalb_full)
+predict(model_acme_2,dekalb_full)
+school_cross_no_dekalb_no_outliers
+##########################################################################
+#####     Predicting with 3 outliers removed, per model summary:     #####
+#####  using DeKalb County, ACME_model_2 estimates 19.07 ACT Score   #####
+#####  using DeKalb County, full_model_2 estimates 19.22 ACT Score   #####
+####           Actual DeKalb County ACT Score was 19.1               #####
+##########################################################################
+#####   SUCCSES! in predicting ACT Composite based on proficiency    #####
+##########################################################################
 
- 
+#### correlation mapping with PerformanceAnalytics package ####
 library(PerformanceAnalytics)
 my_data <- school_cross[, c('ACT_Composite', 'AlgII', 'Math', 'Chemistry', 'ELA')]
 chart.Correlation(my_data, histogram=TRUE, pch=21)
